3a18dbe534cbae57f3bb1e872ce7a9da7077edc2
Yves Faems <yves.faems@gmail.com>
2022-01-16 13:56:58 -0800

    position footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './components/Navbar'
+
+import styles from './App.module.css';
+
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
-      <div>
+      <div className={styles.container}>
         <Navbar />
         <Main />
         <Footer />
